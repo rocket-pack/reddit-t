228682bfe61ab92424eb281632201527e4d8f1db
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 10:48:48 +1200

    Handle subreddit/multireddit listings

--- app/scripts/routes/post_route.js
+++ app/scripts/routes/post_route.js
@@ -5,6 +5,9 @@ RedditT.PostRoute = Ember.Route.extend({
   model: function(params) {
     return $.getJSON("https://pay.reddit.com/" + params.post_slug + ".json", {limit: 1})
           .then(function(api_response) {
+            // Posts are returned within an array, subreddit/multireddit listings are not
+            if (api_response instanceof Array) api_response = api_response[0];
+            return api_response.data.children[0].data;
           })
           .then(function(post) {
             model = RedditT.Post.create(post);
