5bbc00e631dc6c2b99e7100ca2cae3b50b8ac3ab
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 10:48:10 +1200

    Add ability to jump to next/previous post

---
+++ app/scripts/controllers/post_controller.js
@@ -0,0 +1,18 @@
+RedditT.PostController = Ember.ObjectController.extend({
+  actions: {
+    goToNextPost: function() {
+      controller = this;
+      this.get('model').next().then(function(next_model) {
+        controller.set('model', next_model);
+        window.scrollTo(0, 0);
+      });
+    },
+    goToPreviousPost: function() {
+      controller = this;
+      this.get('model').previous().then(function(previous_model) {
+        controller.set('model', previous_model);
+        window.scrollTo(0, 0);
+      });
+    }
+  }
+})
--- app/scripts/models/post.js
+++ app/scripts/models/post.js
@@ -6,4 +6,18 @@ RedditT.Post =  Ember.Object.extend({
     var decoded = $('<textarea />').html(this.get('selftext_html')).val();
     return Ember.String.htmlSafe(decoded);
   }.property('selftext_html'),
+  next: function() {
+    return $.getJSON(this.get('subreddit_url') + '.json', {limit: 1, after: this.get('name')})
+            .then(function(api_response) {
+              return RedditT.Post.create(api_response.data.children[0].data);
+            });
+  },
+  previous: function() {
+    current_model = this;
+    return $.getJSON(this.get('subreddit_url') + '.json', {limit: 1, before: this.get('name')})
+          .then(function(api_response) {
+            if (api_response.data.children.length == 0) return current_model;
+            return RedditT.Post.create(api_response.data.children[0].data);
+          });
+  }
 })
