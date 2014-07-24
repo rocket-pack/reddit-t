db3120ecebc1b73a66fd242278fefdf2dc5ad748
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 10:48:33 +1200

    Only ever return a single post at a time

--- app/scripts/routes/post_route.js
+++ app/scripts/routes/post_route.js
@@ -3,7 +3,7 @@ RedditT.PostRoute = Ember.Route.extend({
     $(document).attr('title', this.modelFor(this.routeName).get('title'));
   },
   model: function(params) {
-    return $.getJSON("https://pay.reddit.com/" + params.post_slug + ".json")
+    return $.getJSON("https://pay.reddit.com/" + params.post_slug + ".json", {limit: 1})
           .then(function(api_response) {
           })
           .then(function(post) {
