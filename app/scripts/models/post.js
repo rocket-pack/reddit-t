cb99cc1ff96ac240db3188fecf0d1a2c528fdc67
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 10:47:35 +1200

    Refactor post body processing
    Move encoded HTML to body processing computed property in model

--- app/scripts/models/post.js
+++ app/scripts/models/post.js
@@ -2,4 +2,8 @@ RedditT.Post =  Ember.Object.extend({
   author_url: function() { return "http://reddit.com/u/" + this.get('author'); }.property('author'),
   comments_url: function() { return this.get('url') + "comments"; }.property('url'),
   subreddit_url: function() { return "https://pay.reddit.com/r/" + this.get('subreddit'); }.property('subreddit'),
+  body: function() {
+    var decoded = $('<textarea />').html(this.get('selftext_html')).val();
+    return Ember.String.htmlSafe(decoded);
+  }.property('selftext_html'),
 })
--- app/scripts/routes/post_route.js
+++ app/scripts/routes/post_route.js
@@ -2,12 +2,6 @@ RedditT.PostRoute = Ember.Route.extend({
   model: function(params) {
     return $.getJSON("https://pay.reddit.com/" + params.post_slug + ".json")
           .then(function(api_response) {
-            return api_response[0].data.children[0].data;
-          })
-          .then(function(post) {
-            decoded = $('<textarea />').html(post.selftext_html).val();
-            post.body = Ember.String.htmlSafe(decoded);
-            return post;
           })
           .then(function(post) {
             model = RedditT.Post.create(post);
