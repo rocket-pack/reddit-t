6e41f2db05727939566e9da638a4281456c58d5a
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 16:24:31 +1200

    Add nginx conf

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,6 +8,7 @@ mkdir -p /home/deployer/.ssh
 mkdir -p /home/deployer/reddit-t
 git init /home/deployer/reddit-t
 cd /home/deployer/reddit-t
+ln -s /home/deployer/reddit-t/config/nginx.conf /etc/nginx/sites-enabled/reddit-t.conf
 git remote add origin git://github.com/rocket-pack/reddit-t.git
 chown -R deployer:deployer .
 echo "cd /home/deployer/reddit-t" > /etc/cron.d/reddit-t
