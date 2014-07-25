aa5b2fb03c93a0103044ea3e84513081cefa8c24
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 16:20:37 +1200

    Update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,13 @@ apt-get update
 apt-get install -y nginx fail2ban varnish unattended-upgrades git-core htop
 adduser --disabled-password --gecos "" deployer
 mkdir -p /home/deployer/.ssh
-git clone 'git://github.com/rocket-pack/reddit-t.git' '/home/deployer/reddit-t'
-chown -R deployer:deployer /home/deployer/reddit-t
-echo 'cd /home/deployer/reddit-t && git pull origin master' > /etc/cron.d/reddit-t
+mkdir -p /home/deployer/reddit-t
+git init /home/deployer/reddit-t
+cd /home/deployer/reddit-t
+git remote add origin git://github.com/rocket-pack/reddit-t.git
+chown -R deployer:deployer .
+echo "cd /home/deployer/reddit-t" > /etc/cron.d/reddit-t
+echo "git pull origin master" >> /etc/cron.d/reddit-t
+bash /etc/cron.d/reddit-t
+
 echo 'Project created. Now log in and add your public key to /home/deployer/.ssh/authorized_keys and then run scripts/secure.sh'
