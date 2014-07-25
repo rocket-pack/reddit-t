5dc13e939aa7560dea7c49b62f81002f5f68429d
Josh McArthur <joshua.mcarthur@gmail.com>
2014-07-25 19:08:15 +1200

    When bootstrapping, remove old config

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,6 +10,7 @@ mkdir -p /home/deployer/reddit-t
 git init /home/deployer/reddit-t
 cd /home/deployer/reddit-t
 ln -sf /home/deployer/reddit-t/conf/nginx.conf /etc/nginx/sites-enabled/reddit-t.conf
+rm /etc/nginx/sites-enabled/default
 git remote add origin git://github.com/rocket-pack/reddit-t.git
 chown -R deployer:deployer .
 echo "cd /home/deployer/reddit-t" > /etc/cron.d/reddit-t
