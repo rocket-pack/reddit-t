8f2b4b148c2858a57137b87c46173ba479b65b0c
Josh McArthur <joshua.mcarthur@gmail.com>
2014-09-06 12:59:00 +1200

    Add tabs from refills

---
+++ app/scripts/vendor/jquey.refills.tabs.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(document).on('click', '.accordion-tabs-minimal li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs-minimal')
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
