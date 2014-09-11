ca2e68e305a20aa776f89c2922f888230de74ca4
Josh McArthur <joshua.mcarthur@gmail.com>
2014-09-12 10:44:34 +1200

    Update Javascript to not show/hide tab content

--- app/scripts/vendor/jquey.refills.tabs.js
+++ app/scripts/vendor/jquey.refills.tabs.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     if (!$(this).hasClass('is-active')) {
       event.preventDefault();
       var accordionTabs = $(this).closest('.accordion-tabs-minimal')
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
+      accordionTabs.find('.is-open').removeClass('is-open')
 
-      $(this).next().toggleClass('is-open').toggle();
+      $(this).next().toggleClass('is-open');
       accordionTabs.find('.is-active').removeClass('is-active');
       $(this).addClass('is-active');
     } else {
